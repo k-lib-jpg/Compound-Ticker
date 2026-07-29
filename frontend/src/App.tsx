187fb216ea8149b17d05d173844e82a15da2e22a
k-lib-jpg <k-liberty080822@outlook.com>
2026-07-29 23:15:32 +0900

    stock page added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -24,8 +24,25 @@ const App = () =>  {
             <li><a href="/">login</a></li>
           </ul>
         </header>
-        
         <h1>{message}</h1>
+        <main>
+        <section>
+          <input
+            type="text"
+            placeholder="銘柄コードを入力"
+          />
+          <button>比較する</button>
+        </section>
+
+        <section>
+          <h2>比較結果</h2>
+          <p>銘柄国：未選択</p>
+          <p>銘柄：未選択</p>
+          <p>総利益：---</p>
+          <p>株価：---</p>
+          <p>配当金：---</p>
+        </section>
+      </main>
       </div>
     </>
   );
